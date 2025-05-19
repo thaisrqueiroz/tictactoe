959f1131dcc8fdad4845e6a0440231c5c9bc87ea
May1704 <vergara.mayleris@gmail.com>
2025-05-19 15:00:38 +0200

    add code for GameController.java

--- src/main/java/controllers/GameController.java
+++ src/main/java/controllers/GameController.java
@@ -1,4 +1,43 @@
 package controllers;
 
+import java.util.Scanner;
+
+import models.Game;
+import views.GameView;
+
 public class GameController {
+    private Game game;
+    private GameView view;
+    private Scanner scanner;
+
+    public GameController(){
+        game = new Game();
+        view = new GameView(game);
+        scanner = new Scanner(System.in);
+    }
+
+    public void StartGame(){
+        view.displayWelcome();
+        while (!game.isGameOver()) {
+            view.displayBoard();
+            view.displayGameStatus();
+            System.out.print("Enter coordinates (row column): ");
+            try {
+                int row = scanner.nextInt();
+                int col = scanner.nextInt();
+            if (!game.makeMove(row,col)) {
+                view.displayInvalidMove();
+            }
+            } catch (Exception e) {
+                scanner.nextLine();
+                view.displayInvalidMove();
+            }
+        }
+        view.displayBoard();
+        view.displayGameStatus();
+        scanner.close();
+    }
 }
+
+
+
